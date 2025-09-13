6cbd8978694ed01644e883319cf6887be4b04f99
Yago rocha <yagodasilvarocha30@gmail.com>
2025-09-13 12:26:10 -0300

    Operacao de Instanciacao da base de dados

---
+++ src/main/java/com/aprendendo/workshopmongo/config/Instatiation.java
@@ -0,0 +1,32 @@
+package com.aprendendo.workshopmongo.config;
+
+import com.aprendendo.workshopmongo.domain.User;
+import com.aprendendo.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instatiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria brown", "maria@gmail.com");
+        User alex = new User(null, "Alex green", "alex@gmail.com");
+        User bob = new User(null, "Bob grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+
+
+
+    }
+}
